7588a14553352be39ab9dcaea54c8d1299b26171
Jimmy Bush <jebush94@hotmail.com>
2016-08-08 11:56:35 -0400

    An Update

--- Main.js
+++ Main.js
@@ -1,3 +1,23 @@
 /**
  * Created by rush on 8/8/16.
  */
+
+function doIt(start, end){
+    for (var i = start; i <= end; i= i + 1){
+        if (i % 15 == 0){
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 == 0){
+            console.log("Fizz");
+        }
+        else if (i % 5 == 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+        }
+
+    }
+}
+
+doIt(1,100);
