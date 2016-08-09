080fa6f1ccfdd0d7b7068831dd01fb061df6c7ee
Jimmy Bush <jebush94@hotmail.com>
2016-08-09 10:37:30 -0400

    Final maybe

--- Main.js
+++ Main.js
@@ -2,22 +2,29 @@
  * Created by rush on 8/8/16.
  */
 
-function doIt(start, end){
-    for (var i = start; i <= end; i= i + 1){
-        if (i % 15 == 0){
-            console.log("FizzBuzz");
-        }
-        else if (i % 3 == 0){
-            console.log("Fizz");
-        }
-        else if (i % 5 == 0){
-            console.log("Buzz");
-        }
-        else{
-            console.log(i);
-        }
+function FizzBuzz (start, end) {
+    this.start = start;
+    this.end = end;
 
+
+    this.doIt = function() {
+        for (var i = this.start; i <= this.end; i++) {
+            if (i % 15 == 0) {
+                console.log("FizzBuzz");
+            } else if (i % 3 == 0) {
+                console.log("Fizz");
+            } else if (i % 5 == 0) {
+                console.log("Buzz");
+            } else {
+                console.log(i);
+            }
+        }
     }
+
 }
 
-doIt(1,100);
+var fizzbuzz = new FizzBuzz(1, 100);
+
+var result = fizzbuzz.doIt();
+
+console.log(result);
